ccdbe9ae84f6d6a817c313b3d73404ed3d32f33c
vivekop0 <sarkarjii534@gamil.com>
2024-02-01 15:27:29 +0530

    added:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,17 @@ import { Todos } from './components/Todos'
 
 function App() {
  
-  const [todos, setTodos] = useState([]);
+  // const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos")
-    .then(async function(res) {
-      const json = await res.json();
-      setTodos(json.todos);
-    })
+  // fetch("http://localhost:3000/todos")
+  //   .then(async function(res) {
+  //     const json = await res.json();
+  //     setTodos(json.todos);
+  //   })
 return(<>
 
   <CreateTodo></CreateTodo>
-  <Todos todos={todos}></Todos>
+  {/* <Todos todos={todos}></Todos> */}
 </>)
 }
 
